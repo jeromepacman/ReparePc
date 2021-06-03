3e1183ebe504da451e4c23c8886cbcb9d29640bc
jeromepacman <jeromepacman@gmail.com>
2021-06-03 14:12:00 +0200

    apache config

--- osm/settings.py
+++ osm/settings.py
@@ -9,9 +9,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/1
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
